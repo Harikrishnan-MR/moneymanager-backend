f82c809b5a4f052e507d7ff5234f89cb337b7ef8
Harikrishnan-MR <mrharikrishnan1216@gmail.com>
2025-09-19 21:45:29 +0530

    First commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin:21-jre
+WORKDIR /app
+COPY target/moneymanager-app.jar moneymanager-v1.0.jar
+EXPOSE 9090
+ENTRYPOINT ["java", "-jar", "moneymanager-v1.0.jar"]
---
+++ src/test/java/com/harikrishnan/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harikrishnan.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
