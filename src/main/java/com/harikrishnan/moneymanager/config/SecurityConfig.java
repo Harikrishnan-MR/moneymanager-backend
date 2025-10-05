c74fae8e92cd840cc7b0a9ace35df31c2d3ddd12
Harikrishnan-MR <mrharikrishnan1216@gmail.com>
2025-10-05 15:11:46 +0530

    configured to allow all header

--- src/main/java/com/harikrishnan/moneymanager/config/SecurityConfig.java
+++ src/main/java/com/harikrishnan/moneymanager/config/SecurityConfig.java
@@ -53,14 +53,15 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(frontendUrl));
+        configuration.setAllowedOriginPatterns(List.of(frontendUrl)); // from env
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization","Content-Type", "Accept"));
+        configuration.setAllowedHeaders(List.of("*"));   // allow all headers
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 
     @Bean
